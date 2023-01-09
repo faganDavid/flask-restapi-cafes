15ea2269a96f0145834d49fd21ed6cbe02f34533
fagadavid <fagan.david3@hotmail.com>
2023-01-09 18:55:05 +0000

    initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,5 @@
 from app import create_app
 
-## HTTP GET - Read Record
-
-## HTTP POST - Create Record
-
-## HTTP PUT/PATCH - Update Record
-
-## HTTP DELETE - Delete Record
-
 app = create_app()
 
 if __name__ == '__main__':
